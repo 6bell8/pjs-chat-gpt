beff777d57ee9ea80f333aba9eaa5aac8d65e720
par jin sung <parkgutime@gmail.com>
2023-06-22 20:05:11 +0900

    [init] pjs-chat-gpt api 연동 중

---
+++ server.js
@@ -0,0 +1,44 @@
+const PORT = 8000;
+const express = require("express");
+// const fetch = require("node-fetch");
+const cors = require("cors");
+const app = express();
+// node js는 fetch require로 불러오는건데, 먼 importㅋㅋ
+
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = "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";
+
+// post 양식을 제출할 때, completions page로 보내서 받기
+// 비동기 함수 때릴 떄, 파라미터 앞에 어싱크
+app.post("/completions", async (req, res) => {
+  const options = {
+    methods: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    // json string으로 바꿔주는 것
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "how are you?" }],
+      max_tokens: 100,
+    }),
+  };
+  try {
+    // 불러들일 때 까지 await
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    // 불러들인 데이터를 json 형태로 변환
+    const data = await response.json();
+    // 서버에서 다시 데이터를 전송
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(PORT, () => console.log(PORT + "서버가 작동 중입니다."));
---
+++ src/App.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+
+import { BsPlusCircleFill } from "react-icons/bs";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import "./index.css";
+
+const App = () => {
+  // backend에서 send한 data를 다시 보내주는 작업
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        // 일단 하드코딩으로 test
+        message: "안녕하세요? 반갑습니다.",
+      }),
+      headers: {
+        "Content-TYPE": "application/json",
+      },
+    };
+    try {
+      // 서버에서 했던 것 처럼 서버에서 전송한 데이터를 json 형태로 가공하는 작업
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>
+          <BsPlusCircleFill className="plus-icon" />
+          새로운 채팅
+        </button>
+        <ul className="history">
+          <li>asd</li>
+        </ul>
+        <nav>
+          <p>Made By Jinsung</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>pjs-chatGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit" onClick={getMessages}>
+              <AiOutlineArrowRight />
+            </div>
+          </div>
+          <p className="info">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            optio vitae molestias. Totam perspiciatis reiciendis ducimus
+            asperiores sed ipsam enim quae, eligendi quasi quos quidem commodi!
+            Esse magni iure sint!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
